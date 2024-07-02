efc67c5faf6da8c3f8a24a172b0e2e5fccc189cc
KishoreVB70 <kishore07kzr@gmail.com>
2024-07-02 21:11:49 +0530

    Styling confirmation for the sidee bar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -5,7 +5,7 @@ const items = [1,2,3];
 
 function SideBar() {
   return (
-    <div className='border border-black h-full w-2/12' >
+    <div className='h-full w-2/12' >
         {items.map(itm => <SideBarList />)}
     </div>
   )
--- src/components/SideBarList.jsx
+++ src/components/SideBarList.jsx
@@ -4,11 +4,11 @@ const items = [1,2,3,4,5];
 
 function SideBarList() {
   return (
-    <div className='flex flex-col border-b border-black p-2 m-2' >
-      <h1 className='font-bold' >Explore</h1>
+    <div className='flex flex-col border-b border-gray-300 p-2 pl-3 m-2' >
+      <h1 className='font-bold mb-2 ' >Explore</h1>
         {items.map(itm => 
-          <div className='flex flex-row hover:cursor-pointer hover:bg-gray-100 w-full' > 
-            <img className='h-8 my-2' src="https://www.iconpacks.net/icons/3/free-fire-flame-icon-5621.png" alt="trending-icon" />
+          <div className='flex flex-row hover:cursor-pointer hover:bg-gray-100 w-full pl-2 items-center hover:rounded-xl' > 
+            <img className='h-8 my-2 mr-5 ' src="https://www.iconpacks.net/icons/3/free-fire-flame-icon-5621.png" alt="trending-icon" />
             <p>Trending</p>
           </div>
         )}
