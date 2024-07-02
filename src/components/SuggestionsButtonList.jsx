e9c10b30a4097cf50df03bbdb62f305cf83e730a
KishoreVB70 <kishore07kzr@gmail.com>
2024-07-02 13:51:35 +0530

    Styled the suggestion button list

--- src/components/SuggestionsButtonList.jsx
+++ src/components/SuggestionsButtonList.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-const cards = [1,2,3,4,5,6,7,8];
+const cards = [1,2,3,4,5,6,7,8, 9, 10, 11 , 12, 13, 14, 15, 16, 17, 18];
 
 function SuggestionsButtonList() {
   return (
-    <div className='flex flex-row h-16' >
+    <div className='flex flex-row h-9 ml-2 my-2' >
         {cards.map(itm => 
-          <button>Gaming</button>
+          <button className='font-semibold mx-3 px-2 bg-gray-100 hover:bg-gray-200 h-full rounded-md border-gray-200 border' >
+            Gaming
+          </button>
         )}
     </div>
   )
