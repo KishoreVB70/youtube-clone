c2d4b79de937525d786a6092807bf162c65b8298
KishoreVB70 <kishore07kzr@gmail.com>
2024-07-02 21:49:26 +0530

    implemented the toggel feature for the side bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import './App.css';
 // import {Outlet} from "react-router-dom"
 import Header from './components/Header';
-import DefaultSideBar from './components/DefaultSideBar';
 import Body from "./components/Body";
+import { SideBarContext } from './utils/SidebarContext';
+import { useState } from 'react';
 
 function App() {
+  const [sideBarState, setSidebarState] = useState(false);
   return (
     <div className='h-full w-full' >
-      <Header />
-      <Body />
+      <SideBarContext.Provider value={{sideBarState: sideBarState, setSidebarState}}>
+        <Header />
+        <Body />
+      </SideBarContext.Provider>
     </div>
   );
 }
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import SuggestionsButtonList from './SuggestionsButtonList'
 import VideoCardsContainer from './VideoCardsContainer'
 import SideBar from './SideBar'
 import DefaultSideBar from './DefaultSideBar'
+import { SideBarContext } from '../utils/SidebarContext'
 
 const Body = () => {
+  const {sideBarState} = useContext(SideBarContext);
   return (
     <div className='flex flex-row w-full h-screen' >
-      {/* <DefaultSideBar /> */}
-      <SideBar />
+        {sideBarState?<SideBar />:<DefaultSideBar />}
       <div className='flex flex-col w-full h-full mx-2' >
+          <h1>{sideBarState}</h1>
           <SuggestionsButtonList />
           <VideoCardsContainer />
       </div>
