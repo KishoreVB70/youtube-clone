c4c8a62be85f8965b6384d235584dfe7673f137b
KishoreVB70 <kishore07kzr@gmail.com>
2024-07-03 06:56:01 +0530

    added router configs

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Header from './components/Header';
 import Body from "./components/Body";
 import { SideBarContext } from './utils/SidebarContext';
 import { useState } from 'react';
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import VideoPage from './components/VideoPage';
+
 
 function App() {
   const [sideBarState, setSidebarState] = useState(false);
@@ -11,10 +14,27 @@ function App() {
     <div className='h-full w-full' >
       <SideBarContext.Provider value={{sideBarState: sideBarState, setSidebarState}}>
         <Header />
-        <Body />
+        <Outlet />
       </SideBarContext.Provider>
     </div>
   );
 }
+
+export const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/video",
+        element: <VideoPage />
+      }
+    ]
+  }
+])
 
 export default App;
