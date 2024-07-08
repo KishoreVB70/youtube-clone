dec346b51acd37556d8b905dba31236533dd4c7c
KishoreVB70 <kishore07kzr@gmail.com>
2024-07-08 13:35:44 +0530

    finished up demoRef

--- src/components/DemoRef.jsx
+++ src/components/DemoRef.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 const DemoRef = () => {
+  const summa = useRef(null);
+  useEffect(() => {
+    summa.current = setInterval(() => console.log(Math.random()), 1000);
+    return () => {
+      clearInterval(summa.current);
+    }
+  }, [])
   return (
     <div className='w-10/12 h-1/4 relative my-5'>
         <div className='w-full h-full border border-black mt-16 m-2 absolute '>
+          <button className='border border-black p-2 m-2' onClick={() => clearInterval(summa.current)}>Stop timeout</button>
         </div>
     </div>
   )
